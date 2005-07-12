5f68e402686d0dbcfbe6c76a57e7c694495250eb
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2005-07-12 12:10:19 +0000

    Move define()
    git-svn-id: http://mu.svn.wordpress.org/trunk@3 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-inst/wp-config-sample.php
+++ wp-inst/wp-config-sample.php
@@ -1,7 +1,4 @@
 <?php
-
-/* $Id: wp-config-sample.php,v 1.2 2005/01/13 23:33:38 donncha Exp $ */
-
 // ** MySQL settings ** //
 define('DB_NAME', 'wordpress');     // The name of the database
 define('DB_USER', 'username');     // Your MySQL username
@@ -10,7 +7,14 @@ define('DB_HOST', 'localhost');     // 99% chance you won't need to change this 
 define('VHOST', 'VHOSTSETTING'); 
 
 
+// Change the prefix if you want to have multiple blogs in a single database.
 $table_prefix  = 'wp_';   // example: 'wp_' or 'b2' or 'mylogin_'
+
+// Change this to localize WordPress.  A corresponding MO file for the
+// chosen language must be installed to wp-includes/languages.
+// For example, install de.mo to wp-includes/languages and set WPLANG to 'de'
+// to enable German language support.
+define ('WPLANG', '');
 
 /* Stop editing */
 
