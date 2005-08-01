9c846c4594204789b7bea8eb86c9da0119e8a6dd
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2005-08-01 09:10:04 +0000

    Added wpmu feeds page
    git-svn-id: http://mu.svn.wordpress.org/trunk@109 7be80a69-a1ef-0310-a953-fb0f7c49ff36

---
+++ wp-inst/wp-admin/wpmu-feeds.php
@@ -0,0 +1,39 @@
+<?php
+require_once('admin.php');
+
+$title = __('WPMU Admin');
+$parent_file = 'wpmu-admin.php';
+require_once('admin-header.php');
+if( $wpblog != 'main' || $user_level < 10) {
+    die( __('<p>You do not have permission to access this page.</p>') );
+}
+if (isset($_GET['updated'])) {
+    ?><div class="updated"><p><strong><?php _e('Options saved.') ?></strong></p></div><?php
+}
+print '<div class="wrap">';
+switch( $_GET[ 'action' ] ) {
+	default: 
+	break;
+}
+
+$customizefeed1 = get_site_option( 'customizefeed1' );
+$customizefeed2 = get_site_option( 'customizefeed2' );
+$dashboardfeed1 = get_site_option( 'dashboardfeed1' );
+$dashboardfeed2 = get_site_option( 'dashboardfeed2' );
+
+?>
+<h2>Dashboard Feeds</h2>
+<p>The dashboard displays two feeds. You can allow your users to customize those feeds to set them to a feed of your own.</p>
+<form action='wpmu-edit.php?action=updatefeeds' method='POST'>
+<table>
+<tr><td valign='top'>Feed 1</td><td><ul><li> User customizable: <input type='radio' name='customizefeed1' value='1'<?php echo $customizefeed1 == 1 ? ' checked' : ''?>> Yes <input type='radio' name='customizefeed1' value='0'<?php echo $customizefeed1 == 0 ? ' checked' : ''?>> No</li>
+			<li> Default Feed URL: <input type='text' name='dashboardfeed1' size='40' value='<?php echo $dashboardfeed1 ?>'></li></ul></td></tr>
+<tr><td valign='top'>Feed 2</td><td><ul><li> User customizable: <input type='radio' name='customizefeed2' value='1'<?php echo $customizefeed2 == 1 ? ' checked' : ''?>> Yes <input type='radio' name='customizefeed2' value='0'<?php echo $customizefeed2 == 0 ? ' checked' : ''?>> No</li>
+			<li> Default Feed URL: <input type='text' name='dashboardfeed2' size='40' value='<?php echo $dashboardfeed2 ?>'></li></ul></td></tr>
+
+</table>
+<input type='submit' value='Update Feeds'>
+</form>
+
+</div>
+<?php include('admin-footer.php'); ?>
