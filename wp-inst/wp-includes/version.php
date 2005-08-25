ef7f2e22370bb21ea9e46f57d0eefeb4d6717873
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2005-08-25 22:09:51 +0000

    Our very own cuddly version!
    git-svn-id: http://mu.svn.wordpress.org/trunk@233 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-inst/wp-includes/version.php
+++ wp-inst/wp-includes/version.php
@@ -2,6 +2,6 @@
 
 // This just holds the version number, in a separate file so we can bump it without cluttering the SVN
 
-$wp_version = '1.6-ALPHA-2-still-dont-use';
+$wp_version = 'MU'; // Let's just avoid confusion
 
 ?>
