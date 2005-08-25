282e905e7c7c7c33d6184bcb4fe4d44ddf1b61c7
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2005-08-25 13:51:13 +0000

    Added simple feedback form
    git-svn-id: http://mu.svn.wordpress.org/trunk@227 7be80a69-a1ef-0310-a953-fb0f7c49ff36

---
+++ wp-inst/wp-admin/wpmu-feedback.php
@@ -0,0 +1,10 @@
+<?php
+require_once('admin.php');
+/*
+function wpmufeedback() {
+	// do something with: $_GET[ 'user_login' ], $_GET[ 'host' ], $_GET[ 'browser' ], $_GET[ 'req' ], $_GET[ 'feedback' ]
+}
+*/
+wpmufeedback();
+echo srand( 5 );
+?>
