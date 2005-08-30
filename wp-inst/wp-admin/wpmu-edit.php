bff296171ab1b02891253ddb65f67908901dec32
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2005-08-30 12:12:30 +0000

    Reformatted
    git-svn-id: http://mu.svn.wordpress.org/trunk@258 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-inst/wp-admin/wpmu-edit.php
+++ wp-inst/wp-admin/wpmu-edit.php
@@ -73,171 +73,158 @@ switch( $_GET[ 'action' ] ) {
 		update_site_option( "dashboardfeed2name", $_POST[ 'dashboardfeed2name' ] );
 		header( "Location: wpmu-feeds.php?updated=true" );
 	break;
-    case "updateblog":
+	case "updateblog":
 		if( is_site_admin() == false ) {
 			die( __('<p>You do not have permission to access this page.</p>') );
 		}
-    	$options_table_name = $wpmuBaseTablePrefix . $id ."_options";
+		$options_table_name = $wpmuBaseTablePrefix . $id ."_options";
 
-    // themes
-    if( is_array( $_POST[ 'theme' ] ) ) {
-	$allowed_themes = $_POST[ 'theme' ];
-	$_POST[ 'option' ][ 'allowed_themes' ] = $_POST[ 'theme' ];
-    }
-    if( is_array( $_POST[ 'option' ] ) ) {
-        while( list( $key, $val ) = each( $_POST[ 'option' ] ) ) { 
-	    if ( is_array($val) || is_object($val) )
-		$val = serialize($val);
+		// themes
+		if( is_array( $_POST[ 'theme' ] ) ) {
+			$allowed_themes = $_POST[ 'theme' ];
+			$_POST[ 'option' ][ 'allowed_themes' ] = $_POST[ 'theme' ];
+		}
+		if( is_array( $_POST[ 'option' ] ) ) {
+			while( list( $key, $val ) = each( $_POST[ 'option' ] ) ) { 
+				if ( is_array($val) || is_object($val) )
+					$val = serialize($val);
 
-	    $query = "SELECT option_id, option_value
-	              FROM   ".$options_table_name."
-		      WHERE  option_name  = '".$key."'";
-	    $opts = $wpdb->get_row( $query, ARRAY_A );
-	    $optvalue = $opts[ 'option_value' ];
-	    $option_id = $opts[ 'option_id' ];
-	    if( $opts == false ) {
-		$query = "INSERT INTO ".$options_table_name." ( `option_id` , `blog_id` , `option_name` , `option_can_override` , `option_type` , `option_value` , `option_width` , `option_height` , `option_description` , `option_admin_level` , `autoload` )
-		          VALUES ( NULL, '0', '".$key."', 'Y', '1', '".$val."', '20', '8', '', '1', 'yes')";
-	        $wpdb->query( $query );
-	    } elseif( $optvalue != $val ) {
-		$query = "UPDATE ".$options_table_name."
-    	                  SET    option_value = '".$val."'
-		          WHERE  option_name  = '".$key."'";
-	        $wpdb->query( $query );
-	    }
-	}
-    }
+				$query = "SELECT option_id, option_value FROM ".$options_table_name." WHERE  option_name  = '".$key."'";
+				$opts = $wpdb->get_row( $query, ARRAY_A );
+				$optvalue = $opts[ 'option_value' ];
+				$option_id = $opts[ 'option_id' ];
+				if( $opts == false ) {
+					$query = "INSERT INTO ".$options_table_name." ( `option_id` , `blog_id` , `option_name` , `option_can_override` , `option_type` , `option_value` , `option_width` , `option_height` , `option_description` , `option_admin_level` , `autoload` ) VALUES ( NULL, '0', '".$key."', 'Y', '1', '".$val."', '20', '8', '', '1', 'yes')";
+					$wpdb->query( $query );
+				} elseif( $optvalue != $val ) {
+					$query = "UPDATE ".$options_table_name." SET option_value = '".$val."' WHERE  option_name  = '".$key."'";
+					$wpdb->query( $query );
+				}
+			}
+		}
 
-    // update blogs table
-    if( $_POST[ 'blog' ][ 'domain' ] != $current_site->domain ) {
-	$query = "UPDATE ".$wpdb->blogs."
-                  SET    domain       = '".$_POST[ 'blog' ][ 'domain' ]."',
-	                 path         = '".$_POST[ 'blog' ][ 'path' ]."',
-	                 registered   = '".$_POST[ 'blog' ][ 'registered' ]."',
-		         last_updated = '".$_POST[ 'blog' ][ 'last_updated' ]."',
-		         is_public    = '".$_POST[ 'blog' ][ 'is_public' ]."'
-	          WHERE  blog_id = '".$id."'";
-        $wpdb->query( $query );
-    }
+		// update blogs table
+		if( $_POST[ 'blog' ][ 'domain' ] != $current_site->domain ) {
+			$query = "UPDATE ".$wpdb->blogs."
+				SET    domain       = '".$_POST[ 'blog' ][ 'domain' ]."',
+				       path         = '".$_POST[ 'blog' ][ 'path' ]."',
+				       registered   = '".$_POST[ 'blog' ][ 'registered' ]."',
+				       last_updated = '".$_POST[ 'blog' ][ 'last_updated' ]."',
+				       is_public    = '".$_POST[ 'blog' ][ 'is_public' ]."'
+					       WHERE  blog_id = '".$id."'";
+			$wpdb->query( $query );
+		}
 
-    // user roles
-    if( is_array( $_POST[ 'role' ] ) == true ) {
-	    $newroles = $_POST[ 'role' ];
-	    reset( $newroles );
-	    while( list( $userid, $role ) = each( $newroles ) ) { 
-		    $role_len = strlen( $role );
-		    $existing_role = $wpdb->get_var( "SELECT meta_value FROM $wpdb->usermeta WHERE user_id = '$userid'  AND meta_key = '" . $wpmuBaseTablePrefix . $id . "_capabilities'" );
-		    if( false == $existing_role ) {
-			    $wpdb->query( "INSERT INTO " . $wpdb->usermeta . "( `umeta_id` , `user_id` , `meta_key` , `meta_value` ) VALUES ( NULL, '$userid', '" . $wpmuBaseTablePrefix . $id . "_capabilities', 'a:1:{s:" . strlen( $role ) . ":\"" . $role . "\";b:1;}')" );
-		    } elseif( $existing_role != "a:1:{s:" . strlen( $role ) . ":\"" . $role . "\";b:1;}" ) {
-			    $wpdb->query( "UPDATE $wpdb->usermeta SET meta_value = 'a:1:{s:" . strlen( $role ) . ":\"" . $role . "\";b:1;}' WHERE user_id = '$userid'  AND meta_key = '" . $wpmuBaseTablePrefix . $id . "_capabilities'" );
-		    }
+		// user roles
+		if( is_array( $_POST[ 'role' ] ) == true ) {
+			$newroles = $_POST[ 'role' ];
+			reset( $newroles );
+			while( list( $userid, $role ) = each( $newroles ) ) { 
+				$role_len = strlen( $role );
+				$existing_role = $wpdb->get_var( "SELECT meta_value FROM $wpdb->usermeta WHERE user_id = '$userid'  AND meta_key = '" . $wpmuBaseTablePrefix . $id . "_capabilities'" );
+				if( false == $existing_role ) {
+					$wpdb->query( "INSERT INTO " . $wpdb->usermeta . "( `umeta_id` , `user_id` , `meta_key` , `meta_value` ) VALUES ( NULL, '$userid', '" . $wpmuBaseTablePrefix . $id . "_capabilities', 'a:1:{s:" . strlen( $role ) . ":\"" . $role . "\";b:1;}')" );
+				} elseif( $existing_role != "a:1:{s:" . strlen( $role ) . ":\"" . $role . "\";b:1;}" ) {
+					$wpdb->query( "UPDATE $wpdb->usermeta SET meta_value = 'a:1:{s:" . strlen( $role ) . ":\"" . $role . "\";b:1;}' WHERE user_id = '$userid'  AND meta_key = '" . $wpmuBaseTablePrefix . $id . "_capabilities'" );
+				}
 
-	    }
-    }
+			}
+		}
 
-    // remove user
-    if( is_array( $_POST[ 'blogusers' ] ) ) {
-	    reset( $_POST[ 'blogusers' ] );
-	    while( list( $key, $val ) = each( $_POST[ 'blogusers' ] ) ) { 
-		    $wpdb->query( "DELETE FROM " . $wpdb->usermeta . " WHERE meta_key = '" . $wpmuBaseTablePrefix . $id . "_capabilities' AND user_id = '" . $key . "'" );
-	    }
-    }
+		// remove user
+		if( is_array( $_POST[ 'blogusers' ] ) ) {
+			reset( $_POST[ 'blogusers' ] );
+			while( list( $key, $val ) = each( $_POST[ 'blogusers' ] ) ) { 
+				$wpdb->query( "DELETE FROM " . $wpdb->usermeta . " WHERE meta_key = '" . $wpmuBaseTablePrefix . $id . "_capabilities' AND user_id = '" . $key . "'" );
+			}
+		}
 
 
-    // add user?
-    if( $_POST[ 'newuser' ] != '' ) {
-	    $newuser = $_POST[ 'newuser' ];
-	    $userid = $wpdb->get_var( "SELECT ID FROM " . $wpdb->users . " WHERE user_login = '$newuser'" );
-	    if( $userid ) {
-		    $user = $wpdb->get_var( "SELECT user_id FROM " . $wpdb->usermeta . " WHERE user_id='$userid' AND meta_key='wp_" . $id . "_capabilities'" );
-		    if( $user == false )
-			    $wpdb->query( "INSERT INTO " . $wpdb->usermeta . "( `umeta_id` , `user_id` , `meta_key` , `meta_value` ) VALUES ( NULL, '$userid', '" . $wpmuBaseTablePrefix . $id . "_capabilities', 'a:1:{s:" . strlen( $_POST[ 'new_role' ] ) . ":\"" . $_POST[ 'new_role' ] . "\";b:1;}')" );
-	    }
-    }
-    header( "Location: wpmu-blogs.php?action=editblog&id=".$id."&updated=true" );
-    break;
-    case "deleteblog":
+		// add user?
+		if( $_POST[ 'newuser' ] != '' ) {
+			$newuser = $_POST[ 'newuser' ];
+			$userid = $wpdb->get_var( "SELECT ID FROM " . $wpdb->users . " WHERE user_login = '$newuser'" );
+			if( $userid ) {
+				$user = $wpdb->get_var( "SELECT user_id FROM " . $wpdb->usermeta . " WHERE user_id='$userid' AND meta_key='wp_" . $id . "_capabilities'" );
+				if( $user == false )
+					$wpdb->query( "INSERT INTO " . $wpdb->usermeta . "( `umeta_id` , `user_id` , `meta_key` , `meta_value` ) VALUES ( NULL, '$userid', '" . $wpmuBaseTablePrefix . $id . "_capabilities', 'a:1:{s:" . strlen( $_POST[ 'new_role' ] ) . ":\"" . $_POST[ 'new_role' ] . "\";b:1;}')" );
+			}
+		}
+		header( "Location: wpmu-blogs.php?action=editblog&id=".$id."&updated=true" );
+	break;
+	case "deleteblog":
 		if( is_site_admin() == false ) {
 			die( __('<p>You do not have permission to access this page.</p>') );
 		}
-	$drop_tables = array( $wpmuBaseTablePrefix . $_GET[ 'id' ] . "_categories",
-			      $wpmuBaseTablePrefix . $_GET[ 'id' ] . "_comments",
-			      $wpmuBaseTablePrefix . $_GET[ 'id' ] . "_linkcategories",
-			      $wpmuBaseTablePrefix . $_GET[ 'id' ] . "_links",
-			      $wpmuBaseTablePrefix . $_GET[ 'id' ] . "_options",
-			      $wpmuBaseTablePrefix . $_GET[ 'id' ] . "_post2cat",
-			      $wpmuBaseTablePrefix . $_GET[ 'id' ] . "_postmeta",
-			      $wpmuBaseTablePrefix . $_GET[ 'id' ] . "_posts",
-			      $wpmuBaseTablePrefix . $_GET[ 'id' ] . "_referer_visitLog",
-			      $wpmuBaseTablePrefix . $_GET[ 'id' ] . "_referer_blacklist" );
-	reset( $drop_tables );
-	while( list( $key, $val ) = each( $drop_tables  ) ) 
-	{ 
-		$wpdb->query( "DROP TABLE $val" );
-	}
-	$wpdb->query( "DELETE FROM ".$wpdb->blogs." WHERE blog_id = '".$_GET[ 'id' ]."'" );
-	header( "Location: wpmu-blogs.php?updated=true" );
-    break;
-    case "deactivateblog":
+		$drop_tables = array( $wpmuBaseTablePrefix . $_GET[ 'id' ] . "_categories",
+			      	$wpmuBaseTablePrefix . $_GET[ 'id' ] . "_comments",
+			      	$wpmuBaseTablePrefix . $_GET[ 'id' ] . "_linkcategories",
+			      	$wpmuBaseTablePrefix . $_GET[ 'id' ] . "_links",
+			      	$wpmuBaseTablePrefix . $_GET[ 'id' ] . "_options",
+			      	$wpmuBaseTablePrefix . $_GET[ 'id' ] . "_post2cat",
+			      	$wpmuBaseTablePrefix . $_GET[ 'id' ] . "_postmeta",
+			      	$wpmuBaseTablePrefix . $_GET[ 'id' ] . "_posts",
+			      	$wpmuBaseTablePrefix . $_GET[ 'id' ] . "_referer_visitLog",
+			      	$wpmuBaseTablePrefix . $_GET[ 'id' ] . "_referer_blacklist" );
+		reset( $drop_tables );
+		while( list( $key, $val ) = each( $drop_tables  ) ) 
+		{ 
+			$wpdb->query( "DROP TABLE $val" );
+		}
+		$wpdb->query( "DELETE FROM ".$wpdb->blogs." WHERE blog_id = '".$_GET[ 'id' ]."'" );
+		header( "Location: wpmu-blogs.php?updated=true" );
+	break;
+	case "deactivateblog":
 		if( is_site_admin() == false ) {
 			die( __('<p>You do not have permission to access this page.</p>') );
 		}
-	$query = "UPDATE ".$wpdb->blogs."
-	          SET    is_public = 'archived'
-	          WHERE  blog_id = '".$_GET[ 'id' ]."'";
-        $wpdb->query( $query );
-	header( "Location: wpmu-blogs.php?updated=true" );
-    break;
-    case "updateuser":
+		$query = "UPDATE ".$wpdb->blogs." SET is_public = 'archived' WHERE  blog_id = '".$_GET[ 'id' ]."'";
+		$wpdb->query( $query );
+		header( "Location: wpmu-blogs.php?updated=true" );
+	break;
+    	case "updateuser":
 		if( is_site_admin() == false ) {
 			die( __('<p>You do not have permission to access this page.</p>') );
 		}
-    unset( $_POST[ 'option' ][ 'ID' ] );
-    if( is_array( $_POST[ 'option' ] ) ) {
-        while( list( $key, $val ) = each( $_POST[ 'option' ] ) ) { 
-    	$query = "UPDATE ".$wpdb->users."
-    	          SET    ".$key." = '".$val."'
-    	          WHERE  ID  = '".$id."'";
-    	$wpdb->query( $query );
-        }
-    }
-    if( is_array( $_POST[ 'meta' ] ) ) {
-        while( list( $key, $val ) = each( $_POST[ 'meta' ] ) ) { 
-    	$query = "UPDATE ".$wpdb->usermeta."
-    	          SET    meta_key = '".$_POST[ 'metaname' ][ $key ]."',
-    		         meta_value = '".$val."'
-    	          WHERE  umeta_id  = '".$key."'";
-    	$wpdb->query( $query );
-        }
-    }
-    if( is_array( $_POST[ 'metadelete' ] ) ) {
-        while( list( $key, $val ) = each( $_POST[ 'metadelete' ] ) ) { 
-	    $query = "DELETE FROM ".$wpdb->usermeta."
-	              WHERE  umeta_id  = '".$key."'";
-	    $wpdb->query( $query );
-        }
-    }
-    header( "Location: wpmu-users.php?action=edit&id=".$id."&updated=true" );
-    break;
-    case "updatethemes":
+		unset( $_POST[ 'option' ][ 'ID' ] );
+		if( is_array( $_POST[ 'option' ] ) ) {
+			while( list( $key, $val ) = each( $_POST[ 'option' ] ) ) { 
+				$query = "UPDATE ".$wpdb->users." SET ".$key." = '".$val."' WHERE  ID  = '".$id."'";
+				$wpdb->query( $query );
+			}
+		}
+		if( is_array( $_POST[ 'meta' ] ) ) {
+			while( list( $key, $val ) = each( $_POST[ 'meta' ] ) ) { 
+				$query = "UPDATE ".$wpdb->usermeta." SET meta_key = '".$_POST[ 'metaname' ][ $key ]."', meta_value = '".$val."' WHERE  umeta_id  = '".$key."'";
+				$wpdb->query( $query );
+			}
+		}
+		if( is_array( $_POST[ 'metadelete' ] ) ) {
+			while( list( $key, $val ) = each( $_POST[ 'metadelete' ] ) ) { 
+				$query = "DELETE FROM ".$wpdb->usermeta." WHERE  umeta_id  = '".$key."'";
+				$wpdb->query( $query );
+			}
+		}
+		header( "Location: wpmu-users.php?action=edit&id=".$id."&updated=true" );
+	break;
+    	case "updatethemes":
 		if( is_site_admin() == false ) {
 			die( __('<p>You do not have permission to access this page.</p>') );
 		}
-    if( is_array( $_POST[ 'theme' ] ) ) {
-	$themes = array_flip( array_keys( get_themes() ) );
-	reset( $themes );
-	while( list( $key, $val ) = each( $themes ) ) 
-	{
-	    if( $_POST[ 'theme' ][ addslashes( $key ) ] == 'enabled' )
-		$allowed_themes[ $key ] = true;
-	}
-	update_site_option( 'allowed_themes', $allowed_themes );
-    }
-    header( "Location: wpmu-themes.php?updated=true" );
-    break;
-    default:
-    header( "Location: wpmu-admin.php" );
-    break;
+    		if( is_array( $_POST[ 'theme' ] ) ) {
+			$themes = array_flip( array_keys( get_themes() ) );
+			reset( $themes );
+			while( list( $key, $val ) = each( $themes ) ) 
+			{
+				if( $_POST[ 'theme' ][ addslashes( $key ) ] == 'enabled' )
+					$allowed_themes[ $key ] = true;
+			}
+			update_site_option( 'allowed_themes', $allowed_themes );
+		}
+		header( "Location: wpmu-themes.php?updated=true" );
+	break;
+	default:
+		header( "Location: wpmu-admin.php" );
+	break;
 }
 ?>
