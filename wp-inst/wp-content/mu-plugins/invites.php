55077691ce668135ec844b0d9e240f41592a7a85
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2005-09-08 11:04:47 +0000

    Added timed invite additions
    git-svn-id: http://mu.svn.wordpress.org/trunk@306 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-inst/wp-content/mu-plugins/invites.php
+++ wp-inst/wp-content/mu-plugins/invites.php
@@ -179,6 +179,27 @@ function admin_menu() {
 function admin_footer() {
 }
 
+add_action('admin_footer', 'timed_invites');
+
+function timed_invites() {
+	global $wpdb;
+
+	$invites_add_days = get_site_option( "invites_add_days", 7 );
+	if( $invites_add_days != 0 ) {
+		$days_registered = $wpdb->get_var( "SELECT TO_DAYS( NOW() ) - TO_DAYS( user_registered ) FROM $wpdb->users WHERE ID = '" . get_current_user_id() . "'" );
+		if( $days_registered % get_site_option( "invites_add_days", 7 ) == 0 ) {
+			$invite_day = get_user_option( "invite_day" );
+			if( $invite_day != $days_registered ) {
+				$invites_left = get_user_option( "invites_left" );
+				if( $invites_left < get_site_option( "invites_per_user" ) ) {
+					update_usermeta( get_current_user_id(), "invites_left", ($invites_left + get_site_option( "invites_add_number", 1 ) ) );
+				}
+				update_usermeta( get_current_user_id(), "invite_day", $days_registered );
+			}
+		}
+	}
+}
+
 function invites_admin_content() {
     global $wpdb;
 
@@ -191,6 +212,15 @@ function invites_admin_content() {
 	update_site_option( "invites_per_user", $_GET[ 'invites_per_user' ] );
 	update_site_option( "invites_default_message", $_GET[ 'invites_default_message' ] );
 	update_site_option( "invites_default_subject", $_GET[ 'invites_default_subject' ] );
+	update_site_option( "invites_add_number", intval( $_GET[ 'invites_add_number' ] ) );
+	update_site_option( "invites_add_days", intval( $_GET[ 'invites_add_days' ] ) );
+	if( $_GET[ 'check_reg_for_invite' ] == 'yes' ) {
+		update_site_option( "check_reg_for_invite", 'yes' );
+	} else {
+		update_site_option( "check_reg_for_invite", 'no' );
+	}
+	?><div id="message" class="updated fade"><p><?php _e('Options saved.') ?></p></div><?php
+
 	break;
 	case "":
 	break;
@@ -205,16 +235,21 @@ function invites_admin_content() {
     ?>
 	<div class='wrap'>
 	<h2>Invite Options</h2>
+	<fieldset class="options">
 	<form method='GET'>
 	<input type="hidden" name="page" value="<?php echo $_GET['page'] ?>"/>
 	<input type='hidden' name='action' value='updateinvitedefaults'>
-	<table>
-	<tr><td valign='top'>Invites Per User:</td><td><input type='text' size='2' maxlength='2' name='invites_per_user' value='<?php echo $invites_per_user ?>'></td></tr>
-	<tr><td valign='top'>Default Subject:</td><td><input type='text' size='70' maxlength='90' name='invites_default_subject' value='<?php echo get_site_option( "invites_default_subject" ) ?>'></td></tr>
-	<tr><td valign='top'>Default Message:</td><td><textarea rows="9" cols="70" name="invites_default_message" tabindex="5" id="defaultmessage"><?php echo str_replace( "\\r\\n", "\n", stripslashes( get_site_option( 'invites_default_message' ) ) ) ?></textarea></td></tr>
+	<table class='editform'>
+	<tr><th scope='row' valign='top'>Invites Per User:</td><td><input type='text' size='2' maxlength='2' name='invites_per_user' value='<?php echo $invites_per_user ?>'></td></tr>
+	<tr><th scope='row' valign='top'>Number of Invites To Add: </td><td><input type='text' size='2' maxlength='2' name='invites_add_number' value='<?php echo get_site_option( "invites_add_number", 1 ) ?>'> (This number of invites will be added to each user every X days.)</td></tr>
+	<tr><th scope='row' valign='top'>Add Invites Every</td><td valign='top'><input type='text' size='2' maxlength='2' name='invites_add_days' value='<?php echo get_site_option( "invites_add_days", 7 ) ?>'> <strong>Days</strong> (0 to disable)</td></tr>
+	<tr><th scope='row' valign='top'>Default Subject:</td><td><input type='text' size='70' maxlength='90' name='invites_default_subject' value='<?php echo get_site_option( "invites_default_subject" ) ?>'></td></tr>
+	<tr><th scope='row' valign='top'>Default Message:</td><td><textarea rows="9" cols="70" name="invites_default_message" tabindex="5" id="defaultmessage"><?php echo str_replace( "\\r\\n", "\n", stripslashes( get_site_option( 'invites_default_message' ) ) ) ?></textarea></td></tr>
+	<tr><th scope='row' valign='top'>Registration - check for invite: </td><td><input type='checkbox' name='check_reg_for_invite' value='yes'<?php if( get_site_option( 'check_reg_for_invite' ) == 'yes' ) echo " checked"; ?>></td></tr>
 	<tr><td valign='top' colspan='2'><input type='submit'></td></tr>
 	</table>
 	</form>
+	</fieldset>
 	</div>
 	<div class='wrap'>
 	<h2>Invite Stats</h2>
