8d0a65085ac979e749957f2803650476018aeb19
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2005-10-07 14:41:01 +0000

    Global categories use new hooks now
    git-svn-id: http://mu.svn.wordpress.org/trunk@391 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-inst/wp-content/mu-plugins/global-categories.php
+++ wp-inst/wp-content/mu-plugins/global-categories.php
@@ -1,29 +1,31 @@
 <?php
 
-function global_categories_filter( $update, $cat_ID, $category_nicename, $cat_name, $rval ) {
+function global_categories( $cat_ID ) {
     global $wpdb;
 
-    $details = $wpdb->get_row( "SELECT * FROM $wpdb->sitecategories WHERE category_nicename = '$category_nicename'" );
-    if( $details == false ) {
-	$res = $wpdb->query( "INSERT INTO $wpdb->sitecategories ( cat_ID, cat_name, category_nicename ) VALUES ( NULL, '$cat_name', '$category_nicename' )" );
+    $details = $wpdb->get_row( "SELECT * FROM $wpdb->categories WHERE cat_ID = '$cat_ID'" );
+    if( $details == false ) { // this should *not* happen ever!
+	    return $cat_ID;
+    }
+    $global_cat = $wpdb->get_row( "SELECT * FROM $wpdb->sitecategories WHERE cat_name = '{$details->cat_name}'" );
+    if( $global_cat == false ) {
+	$res = $wpdb->query( "INSERT INTO $wpdb->sitecategories ( cat_ID, cat_name, category_nicename ) VALUES ( NULL, '{$details->cat_name}', '{$details->category_nicename}' )" );
 	$newcat_ID = $wpdb->insert_id;
 	$wpdb->query( "UPDATE $wpdb->categories SET cat_ID = '$newcat_ID' WHERE cat_ID = '$cat_ID'" );
 	$wpdb->query( "UPDATE $wpdb->post2cat   SET category_id = '$newcat_ID' WHERE category_id = '$cat_ID'" );
 	$cat_ID = $newcat_ID;
 	if( get_option( "default_category" ) == $cat_ID )
 		update_option( "default_category", $newcat_ID );
-    } elseif( $details->cat_ID != $cat_ID ) {
-	$wpdb->query( "UPDATE $wpdb->categories SET cat_ID = '$details->cat_ID' WHERE cat_ID = '$cat_ID'" );
-	$wpdb->query( "UPDATE $wpdb->post2cat   SET category_id = '$details->cat_ID' WHERE category_id = '$cat_ID'" );
+    } elseif( $global_cat->cat_ID != $cat_ID ) {
+	$wpdb->query( "UPDATE $wpdb->categories SET cat_ID = '{$global_cat->cat_ID}' WHERE cat_ID = '$cat_ID'" );
+	$wpdb->query( "UPDATE $wpdb->post2cat   SET category_id = '{$global_cat->cat_ID}' WHERE category_id = '$cat_ID'" );
 	if( get_option( "default_category" ) == $cat_ID )
-		update_option( "default_category", $details->cat_ID );
-	$cat_ID = $details->cat_ID;
+		update_option( "default_category", $global_cat->cat_ID );
+	$cat_ID = $global_cat->cat_ID;
     }
 
-    if( $update == false )
-	$rval = $cat_ID;
-
-    return array( $update, $cat_ID, $category_nicename, $cat_name, $rval );
+    return $cat_ID;
 }
-add_filter('new_category', 'global_categories_filter', 10, 5 );
+add_action( 'edit_category', 'global_categories' );
+add_action( 'add_category', 'global_categories' );
 ?>
