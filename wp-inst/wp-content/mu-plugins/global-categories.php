ae5f9e9dac7affd41cdf59498ec4fe857c0a697a
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2005-10-07 18:20:27 +0000

    Escape sql
    git-svn-id: http://mu.svn.wordpress.org/trunk@392 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-inst/wp-content/mu-plugins/global-categories.php
+++ wp-inst/wp-content/mu-plugins/global-categories.php
@@ -9,13 +9,15 @@ function global_categories( $cat_ID ) {
     }
     $global_cat = $wpdb->get_row( "SELECT * FROM $wpdb->sitecategories WHERE cat_name = '{$details->cat_name}'" );
     if( $global_cat == false ) {
-	$res = $wpdb->query( "INSERT INTO $wpdb->sitecategories ( cat_ID, cat_name, category_nicename ) VALUES ( NULL, '{$details->cat_name}', '{$details->category_nicename}' )" );
+	$res = $wpdb->query( "INSERT INTO $wpdb->sitecategories ( cat_ID, cat_name, category_nicename ) VALUES ( NULL, '" . $wpdb->escape( $details->cat_name ) . "', '" . $wpdb->escape( $details->category_nicename ) . "' )" );
 	$newcat_ID = $wpdb->insert_id;
-	$wpdb->query( "UPDATE $wpdb->categories SET cat_ID = '$newcat_ID' WHERE cat_ID = '$cat_ID'" );
-	$wpdb->query( "UPDATE $wpdb->post2cat   SET category_id = '$newcat_ID' WHERE category_id = '$cat_ID'" );
-	$cat_ID = $newcat_ID;
-	if( get_option( "default_category" ) == $cat_ID )
-		update_option( "default_category", $newcat_ID );
+	if( $newcat_ID != 0 ) { // bad things happen when cat_id == 0
+		$wpdb->query( "UPDATE $wpdb->categories SET cat_ID = '$newcat_ID' WHERE cat_ID = '$cat_ID'" );
+		$wpdb->query( "UPDATE $wpdb->post2cat   SET category_id = '$newcat_ID' WHERE category_id = '$cat_ID'" );
+		$cat_ID = $newcat_ID;
+		if( get_option( "default_category" ) == $cat_ID )
+			update_option( "default_category", $newcat_ID );
+	}
     } elseif( $global_cat->cat_ID != $cat_ID ) {
 	$wpdb->query( "UPDATE $wpdb->categories SET cat_ID = '{$global_cat->cat_ID}' WHERE cat_ID = '$cat_ID'" );
 	$wpdb->query( "UPDATE $wpdb->post2cat   SET category_id = '{$global_cat->cat_ID}' WHERE category_id = '$cat_ID'" );
