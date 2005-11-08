61ab8671ee9744e9e49919168d2e1325df05f354
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2005-11-08 08:42:38 +0000

    WP Merge - caching
    git-svn-id: http://mu.svn.wordpress.org/trunk@423 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-inst/wp-includes/pluggable-functions.php
+++ wp-inst/wp-includes/pluggable-functions.php
@@ -27,16 +27,18 @@ endif;
 
 if ( !function_exists('get_userdata') ) :
 function get_userdata( $user_id ) {
-	global $wpdb, $cache_userdata;
+	global $wpdb;
 	$user_id = (int) $user_id;
 	if ( $user_id == 0 )
 		return false;
 
-	if ( isset( $cache_userdata[$user_id] ) ) 
-		return $cache_userdata[$user_id];
+	$user = wp_cache_get($user_id, 'users');
+	
+	if ( $user )
+		return $user;
 
-	if ( !$user = $wpdb->get_row("SELECT * FROM $wpdb->users WHERE ID = '$user_id'") )
-		return $cache_userdata[$user_id] = false;
+	if ( !$user = $wpdb->get_row("SELECT * FROM $wpdb->users WHERE ID = '$user_id' LIMIT 1") )
+		return false;
 
 	$metavalues = $wpdb->get_results("SELECT meta_key, meta_value FROM $wpdb->usermeta WHERE user_id = '$user_id'");
 
@@ -51,10 +53,10 @@ function get_userdata( $user_id ) {
 			$user->user_level = $meta->meta_value;
 	}
 
-	$cache_userdata[$user_id] = $user;
-	$cache_userdata[$cache_userdata[$user_id]->user_login] =& $cache_userdata[$user_id];
-
-	return $cache_userdata[$user_id];
+	wp_cache_add($user_id, $user, 'users');
+	wp_cache_add($user->user_login, $user, 'users');
+	
+	return $user;
 }
 endif;
 
@@ -95,9 +97,10 @@ function get_userdatabylogin($user_login) {
 
 	if ( empty( $user_login ) )
 		return false;
-
-	if ( isset( $cache_userdata[$user_login] ) )
-		return $cache_userdata[$user_login];
+		
+	$userdata = wp_cache_get($user_login, 'users');
+	if ( $userdata )
+		return $userdata;
 
 	if ( !$user = $wpdb->get_row("SELECT * FROM $wpdb->users WHERE user_login = '$user_login'") )
 		return $cache_userdata[$user_login] = false;
@@ -152,7 +155,8 @@ function wp_login($username, $password, $already_md5 = false) {
 		return false;
 	}
 
-	$login = $wpdb->get_row("SELECT ID, user_login, user_pass FROM $wpdb->users WHERE user_login = '$username'");
+	$login = get_userdatabylogin($username);
+	//$login = $wpdb->get_row("SELECT ID, user_login, user_pass FROM $wpdb->users WHERE user_login = '$username'");
 
 	if (!$login) {
 		$error = __('<strong>Error</strong>: Wrong username.');
@@ -244,8 +248,8 @@ if ( ! function_exists('wp_notify_postauthor') ) :
 function wp_notify_postauthor($comment_id, $comment_type='') {
 	global $wpdb;
     
-	$comment = $wpdb->get_row("SELECT * FROM $wpdb->comments WHERE comment_ID='$comment_id' LIMIT 1");
-	$post    = $wpdb->get_row("SELECT * FROM $wpdb->posts WHERE ID = '$comment->comment_post_ID' LIMIT 1");
+	$comment = get_comment($comment_id);
+	$post    = get_post($comment->comment_post_ID);
 	$user    = get_userdata( $post->post_author );
 
 	if ('' == $user->user_email) return false; // If there's no email to send the comment to
--- wp-inst/wp-settings.php
+++ wp-inst/wp-settings.php
@@ -21,6 +21,9 @@ unregister_GLOBALS();
 $HTTP_USER_AGENT = getenv('HTTP_USER_AGENT');
 unset( $wp_filter, $cache_userdata, $cache_lastcommentmodified, $cache_lastpostdate, $cache_settings, $category_cache, $cache_categories );
 
+if ( ! isset($blog_id) )
+	$blog_id = 1;
+
 // Fix for IIS, which doesn't set REQUEST_URI
 if ( empty( $_SERVER['REQUEST_URI'] ) ) {
 	$_SERVER['REQUEST_URI'] = $_SERVER['SCRIPT_NAME']; // Does this work under CGI?
@@ -66,7 +69,7 @@ $wpdb->sitecategories	= $table_prefix . 'sitecategories';
 // find out what tables to use from $wpblog
 $wpdb->hide_errors();
 
-$domain = $_SERVER['HTTP_HOST'];
+$domain = addslashes($_SERVER['HTTP_HOST']);
 if( substr( $domain, 0, 4 ) == 'www.' )
 	$domain = substr( $domain, 4 );
 
@@ -181,7 +184,7 @@ if ( defined('CUSTOM_USER_TABLE') )
 	$wpdb->users = CUSTOM_USER_TABLE;
 if ( defined('CUSTOM_USER_META_TABLE') )
 	$wpdb->usermeta = CUSTOM_USER_META_TABLE;
-	
+
 // We're going to need to keep this around for a few months even though we're not using it internally
 
 $tableposts = $wpdb->posts;
@@ -193,6 +196,17 @@ $tablelinks = $wpdb->links;
 $tablelinkcategories = $wpdb->linkcategories;
 $tableoptions = $wpdb->options;
 $tablepostmeta = $wpdb->postmeta;
+
+if ( file_exists(ABSPATH . 'wp-content/object-cache.php') )
+	require (ABSPATH . 'wp-content/object-cache.php');
+else
+	require (ABSPATH . WPINC . '/cache.php');
+
+// For now, disable persistent caching by default.  To enable, comment out
+// the following line.
+define('DISABLE_CACHE', true);
+
+wp_cache_init();
 
 $wp_filters = array();
 
@@ -206,7 +220,8 @@ require (ABSPATH . WPINC . '/default-filters.php');
 require_once (ABSPATH . WPINC . '/wp-l10n.php');
 
 $wpdb->hide_errors();
-if ( !update_category_cache() && (!strstr($_SERVER['PHP_SELF'], 'install.php') && !defined('WP_INSTALLING')) ) {
+$db_check = $wpdb->get_var("SELECT option_value FROM $wpdb->options WHERE option_name = 'siteurl'");
+if ( !$db_check && (!strstr($_SERVER['PHP_SELF'], 'install.php') && !defined('WP_INSTALLING')) ) {
 	if ( strstr($_SERVER['PHP_SELF'], 'wp-admin') )
 		$link = 'install.php';
 	else
@@ -329,6 +344,7 @@ if ( file_exists(TEMPLATEPATH . "/functions.php") )
 	include(TEMPLATEPATH . "/functions.php");
 
 function shutdown_action_hook() {
+	wp_cache_close();
 	do_action('shutdown');
 }
 register_shutdown_function('shutdown_action_hook');
