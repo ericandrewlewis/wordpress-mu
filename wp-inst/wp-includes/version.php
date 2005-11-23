486570959b993a029d65137075e4fcf89ad7e5ce
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2005-11-23 11:52:49 +0000

    WP Merge
    git-svn-id: http://mu.svn.wordpress.org/trunk@445 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-inst/wp-includes/version.php
+++ wp-inst/wp-includes/version.php
@@ -3,6 +3,6 @@
 // This just holds the version number, in a separate file so we can bump it without cluttering the SVN
 
 $wp_version = 'MU'; // Let's just avoid confusion
-$wp_db_version = 3183;
+$wp_db_version = 3199;
 
 ?>
