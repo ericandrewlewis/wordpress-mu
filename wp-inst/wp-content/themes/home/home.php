3f0680ee7dc25af81c50abab51bd6cf368be0cff
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2006-06-19 16:19:59 +0000

    Added  list of main blog posts
    git-svn-id: http://mu.svn.wordpress.org/trunk@573 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-inst/wp-content/themes/home/home.php
+++ wp-inst/wp-content/themes/home/home.php
@@ -5,6 +5,16 @@
 	<h2>WordPress &micro;</h2>
 	<p>This is a <a href="http://mu.wordpress.org/">WordPress Mu</a> powered site.</p>
 	<p>You can: <ul><li> <a href="wp-login.php">Login</a> </li><li> <a href="wp-signup.php">Create a new blog</a></li><li> Edit this file at <code>wp-content/themes/home/home.php</code> with your favourite text editor and customize this screen.</li></ul></p>
+<h3>News Blog</h3>
+
+<ul>
+<?php 
+query_posts('showposts=7');
+if (have_posts()) : ?><?php while (have_posts()) : the_post(); ?>
+<li><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title();?> </a></li>
+<?php endwhile; ?><?php endif; ?>
+</ul>
+
 	<?php if( constant( 'VHOST' ) == 'no' ) :?>
 		<h3>Virtual Hosts</h3>
 		<p>As you are not using virtual hosts, it is not possible to post anything to this primary blog because the URLs of your posts could potentially conflict with weblogs living at this site.</p>
