24ef516a90814b7025d0a9d42cb801d1ac1961b3
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2006-07-11 17:10:05 +0000

    Ooops. Forgot ";" (fixes #74)
    git-svn-id: http://mu.svn.wordpress.org/trunk@656 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- index-install.php
+++ index-install.php
@@ -5,7 +5,7 @@ if( strpos( $_SERVER["REQUEST_URI"], 'index-install.php' ) ) {
 	die();
 }
 if( $_SERVER[ 'HTTP_HOST' ] == 'localhost' ) {
-	die( "<h2>Warning!</h2> Installing to http://localhost/ is not supported. Please use <a href='http://localhost.localdomain/'>http://localhost.localdomain/</a> instead." )
+	die( "<h2>Warning!</h2> Installing to http://localhost/ is not supported. Please use <a href='http://localhost.localdomain/'>http://localhost.localdomain/</a> instead." );
 }
 define('WP_INSTALLING', true);
 
