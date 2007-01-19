2aec1f0d91bc2e96787f1182a3edadca15a3bc12
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2007-01-19 13:07:48 +0000

    WP Merge to rev 4771
    git-svn-id: http://mu.svn.wordpress.org/trunk@852 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-includes/script-loader.php
+++ wp-includes/script-loader.php
@@ -10,18 +10,18 @@ class WP_Scripts {
 	}
 
 	function default_scripts() {
-		$this->add( 'dbx', '/wp-includes/js/dbx.compressed.js', false, '2.05' );
-		$this->add( 'fat', '/wp-includes/js/fat.compressed.js', false, '1.0-RC1_3660' );
-		$this->add( 'sack', '/wp-includes/js/tw-sack.compressed.js', false, '1.6.1' );
-		$this->add( 'quicktags', '/wp-includes/js/quicktags.compressed.js', false, '3517' );
+		$this->add( 'dbx', '/wp-includes/js/dbx.js', false, '2.05' );
+		$this->add( 'fat', '/wp-includes/js/fat.js', false, '1.0-RC1_3660' );
+		$this->add( 'sack', '/wp-includes/js/tw-sack.js', false, '1.6.1' );
+		$this->add( 'quicktags', '/wp-includes/js/quicktags.js', false, '3517' );
 		$this->add( 'colorpicker', '/wp-includes/js/colorpicker.js', false, '3517' );
 		$this->add( 'tiny_mce', '/wp-includes/js/tinymce/tiny_mce_gzip.php', false, '20061113' );
 		$mce_config = apply_filters('tiny_mce_config_url', '/wp-includes/js/tinymce/tiny_mce_config.php');
 		$this->add( 'wp_tiny_mce', $mce_config, array('tiny_mce'), '20061113' );
 		$this->add( 'prototype', '/wp-includes/js/prototype.js', false, '1.5.0');
 		$this->add( 'autosave', '/wp-includes/js/autosave-js.php', array('prototype', 'sack'), '20070116');
-		$this->add( 'wp-ajax', '/wp-includes/js/wp-ajax-js.php', array('prototype'), '4459');
-		$this->add( 'listman', '/wp-includes/js/list-manipulation-js.php', array('wp-ajax', 'fat'), '4583');
+		$this->add( 'wp-ajax', '/wp-includes/js/wp-ajax-js.php', array('prototype'), '20070118');
+		$this->add( 'listman', '/wp-includes/js/list-manipulation-js.php', array('wp-ajax', 'fat'), '20070118');
 		$this->add( 'scriptaculous-root', '/wp-includes/js/scriptaculous/wp-scriptaculous.js', array('prototype'), '1.6.1');
 		$this->add( 'scriptaculous-builder', '/wp-includes/js/scriptaculous/builder.js', array('scriptaculous-root'), '1.6.1');
 		$this->add( 'scriptaculous-dragdrop', '/wp-includes/js/scriptaculous/dragdrop.js', array('scriptaculous-builder'), '1.6.1');
@@ -29,16 +29,16 @@ class WP_Scripts {
 		$this->add( 'scriptaculous-slider', '/wp-includes/js/scriptaculous/slider.js', array('scriptaculous-effects'), '1.6.1');
 		$this->add( 'scriptaculous-controls', '/wp-includes/js/scriptaculous/controls.js', array('scriptaculous-root'), '1.6.1');
 		$this->add( 'scriptaculous', '', array('scriptaculous-dragdrop', 'scriptaculous-slider', 'scriptaculous-controls'), '1.6.1');
-		$this->add( 'cropper', '/wp-includes/js/crop/cropper.js', array('scriptaculous-dragdrop'), '1');
+		$this->add( 'cropper', '/wp-includes/js/crop/cropper.js', array('scriptaculous-dragdrop'), '20070118');
 		if ( is_admin() ) {
 			$this->add( 'dbx-admin-key', '/wp-admin/dbx-admin-key-js.php', array('dbx'), '3651' );
-			$this->add( 'ajaxcat', '/wp-admin/cat-js.php', array('listman'), '3684' );
+			$this->add( 'ajaxcat', '/wp-admin/cat-js.php', array('listman'), '20070118' );
 			$this->add( 'admin-categories', '/wp-admin/categories.js', array('listman'), '3684' );
 			$this->add( 'admin-custom-fields', '/wp-admin/custom-fields.js', array('listman'), '3733' );
 			$this->add( 'admin-comments', '/wp-admin/edit-comments.js', array('listman'), '3847' );
 			$this->add( 'admin-users', '/wp-admin/users.js', array('listman'), '4583' );
 			$this->add( 'xfn', '/wp-admin/xfn.js', false, '3517' );
-			$this->add( 'upload', '/wp-admin/upload-js.php', array('prototype'), '20070116' );
+			$this->add( 'upload', '/wp-admin/upload-js.php', array('prototype'), '20070118' );
 		}
 	}
 
