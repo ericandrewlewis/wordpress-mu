4db74486345b99655f5414070d6e504f1a3e0995
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2007-06-20 14:10:15 +0000

    Let's just bump this and be done with it
    git-svn-id: http://mu.svn.wordpress.org/trunk@1004 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -2,7 +2,7 @@
 
 // This holds the version number in a separate file so we can bump it without cluttering the SVN
 
-$wp_version = 'wordpress-mu-1.2.1'; // Let's just avoid confusion
+$wp_version = 'wordpress-mu-1.2.3'; // Let's just avoid confusion
 $wp_db_version = 5200;
 
 ?>
