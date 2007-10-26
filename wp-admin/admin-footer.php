8b05255a401cbec80dd591bf112805cd1c507312
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2007-10-26 16:53:44 +0000

    Removed $upgrade, not used here, fixes #477
    git-svn-id: http://mu.svn.wordpress.org/trunk@1135 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-admin/admin-footer.php
+++ wp-admin/admin-footer.php
@@ -2,7 +2,6 @@
 <div id="footer">
 <p><?php
 
-$upgrade = apply_filters( 'update_footer', '' );
 echo __('Thank you for creating with <a href="http://mu.wordpress.org/">WordPress MU</a>').' | '.__('<a href="http://mu.wordpress.org/docs/">Documentation</a>') ?></p>
 </div>
 <?php do_action('admin_footer', ''); ?>
