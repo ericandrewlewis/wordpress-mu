dd101d1f49e20b1d940884b865c35cd5eeb60f0f
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2007-11-20 15:28:35 +0000

    Make footer text filterable. Admins can insert their own forum and documentation links
    git-svn-id: http://mu.svn.wordpress.org/trunk@1156 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-admin/admin-footer.php
+++ wp-admin/admin-footer.php
@@ -1,8 +1,9 @@
 
 <div id="footer">
 <p><?php
-
-echo __('Thank you for creating with <a href="http://mu.wordpress.org/">WordPress MU</a>').' | '.__('<a href="http://mu.wordpress.org/docs/">Documentation</a>') ?></p>
+$footer_text = __('Thank you for creating with <a href="http://mu.wordpress.org/">WordPress MU</a>').' | '.__('<a href="http://mu.wordpress.org/docs/">Documentation</a>');
+$footer_text = apply_filters( 'admin_footer_text', $footer_text );
+?></p>
 </div>
 <?php do_action('admin_footer', ''); ?>
 <script type="text/javascript">if(typeof wpOnload=='function')wpOnload();</script>
