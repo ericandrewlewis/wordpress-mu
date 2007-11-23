6a725b7085be5cfca4841a1fd7abb77eb2d9709e
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2007-11-23 12:10:11 +0000

    Echo the footer text, props chmac, fixes #507
    git-svn-id: http://mu.svn.wordpress.org/trunk@1163 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-admin/admin-footer.php
+++ wp-admin/admin-footer.php
@@ -2,7 +2,7 @@
 <div id="footer">
 <?php
 $footer_text = '<p>' . __('Thank you for creating with <a href="http://mu.wordpress.org/">WordPress MU</a>') . ' | ' . __('<a href="http://mu.wordpress.org/docs/">Documentation</a>') . '</p>';
-$footer_text = apply_filters( 'admin_footer_text', $footer_text );
+echo apply_filters( 'admin_footer_text', $footer_text );
 ?>
 </div>
 <?php do_action('admin_footer', ''); ?>
