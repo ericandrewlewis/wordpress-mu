f41ef6e4ae856840ef63b01b4422e33d71be2871
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2007-11-23 12:22:07 +0000

    Use MUPLUGINDIR, props chmac, fixes #508
    git-svn-id: http://mu.svn.wordpress.org/trunk@1164 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-admin/admin.php
+++ wp-admin/admin.php
@@ -58,7 +58,7 @@ if (isset($plugin_page)) {
 			wp_die(__('Invalid plugin page'));
 		}
 
-		if (! file_exists(ABSPATH . PLUGINDIR . "/$plugin_page") && ! file_exists(ABSPATH . "wp-content/mu-plugins/$plugin_page"))
+		if (! file_exists(ABSPATH . PLUGINDIR . "/$plugin_page") && ! file_exists(ABSPATH . MUPLUGINDIR . "/$plugin_page"))
 			wp_die(sprintf(__('Cannot load %s.'), htmlentities($plugin_page)));
 
 		do_action('load-' . $plugin_page);
@@ -66,8 +66,8 @@ if (isset($plugin_page)) {
 		if (! isset($_GET['noheader']))
 			require_once(ABSPATH . '/wp-admin/admin-header.php');
 
-		if ( file_exists(ABSPATH . "wp-content/mu-plugins/$plugin_page") )
-			include(ABSPATH . "wp-content/mu-plugins/$plugin_page");
+		if ( file_exists(ABSPATH . MUPLUGINDIR . "/$plugin_page") )
+			include(ABSPATH . MUPLUGINDIR . "/$plugin_page");
 		else
 			include(ABSPATH . PLUGINDIR . "/$plugin_page");
 	}
