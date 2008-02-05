6c20e52cb82052627bbd8959937627066e5deccd
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2008-02-05 15:34:44 +0000

    Merge with WP 2.3.3
    git-svn-id: http://mu.svn.wordpress.org/trunk@1196 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-includes/version.php
+++ wp-includes/version.php
@@ -2,8 +2,8 @@
 
 // This holds the version number in a separate file so we can bump it without cluttering the SVN
 
-$wp_version = '2.3.2';
-$wpmu_version = '1.3.2';
+$wp_version = '2.3.3';
+$wpmu_version = '1.3.3';
 $wp_db_version = 6124;
 
 ?>
