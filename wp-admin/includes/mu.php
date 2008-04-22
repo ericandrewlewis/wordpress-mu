530205ea896022bec89d930546b961e574432094
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2008-04-22 15:56:15 +0000

    Remove plugins widget from dashboard as users won't be able to use them anyway, fixes #599
    git-svn-id: http://mu.svn.wordpress.org/trunk@1257 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-admin/includes/mu.php
+++ wp-admin/includes/mu.php
@@ -655,4 +655,9 @@ function secret_salt_warning() {
 }
 add_action( 'admin_notices', 'secret_salt_warning' );
 
+function mu_dashboard() {
+	unregister_sidebar_widget( 'dashboard_plugins' );
+}
+add_action( 'wp_dashboard_setup', 'mu_dashboard' );
+
 ?>
