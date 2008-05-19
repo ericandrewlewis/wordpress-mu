7fb787ed6e46a05f78b4c6785eefb453518b6f43
ryan <ryan@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2008-05-19 22:17:49 +0000

    Put checked categories at the top of the checklist. Props mdawaffe. see #WP7000
    git-svn-id: http://mu.svn.wordpress.org/trunk@1299 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-admin/edit-form-advanced.php
+++ wp-admin/edit-form-advanced.php
@@ -241,7 +241,7 @@ endif; ?>
 
 <div id="categories-all" class="ui-tabs-panel">
 	<ul id="categorychecklist" class="list:category categorychecklist form-no-clear">
-		<?php wp_category_checklist($post_ID) ?>
+		<?php wp_category_checklist($post->ID, false, false, $popular_ids) ?>
 	</ul>
 </div>
 
