e7b8f5f2e8f6f0f144c30b01a2d59da6c1b96058
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2008-08-25 15:34:01 +0000

    Locale changes, minor code changes. from patches on #708, props momo360modena
    git-svn-id: http://mu.svn.wordpress.org/trunk@1442 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-admin/wpmu-themes.php
+++ wp-admin/wpmu-themes.php
@@ -33,17 +33,16 @@ $allowed_themes = get_site_allowed_themes();
 			</thead>
 			<tbody id="plugins">
 			<?php
-			$total_theme_count = 0;
-			$activated_themes_count = 0;
-			foreach( (array) $themes as $key => $theme ) :
-                                $total_theme_count++;
+			$total_theme_count = $activated_themes_count = 0;
+			foreach( (array) $themes as $key => $theme ) {
+				$total_theme_count++;
 				$theme_key = wp_specialchars($theme['Stylesheet']);
 				$class = ('alt' == $class) ? '' : 'alt';
 				$class1 = $enabled = $disabled = '';
 		
 				if( isset( $allowed_themes[ $theme_key ] ) == true ) {
 					$enabled = 'checked="checked" ';
-                                        $activated_themes_count++;
+					$activated_themes_count++;
 					$class1 = ' active';
 				} else {
 					$disabled = 'checked="checked" ';
@@ -59,15 +58,20 @@ $allowed_themes = get_site_allowed_themes();
 					<td><?php echo $theme['Version'] ?></td>
 					<td><?php echo $theme['Description'] ?></td>
 				</tr> 
-			<?php endforeach; ?>
+			<?php } ?>
 			</tbody>
 		</table>
 
 		<p class="submit">
 			<input type='submit' value='<?php _e('Update Themes &raquo;') ?>' /></p>
 	</form>
-<p>Total Themes Installed: <?php echo $total_theme_count; ?><br />
-Themes Activated: <?php echo $activated_themes_count; ?></p>
+	
+	<h3><?php _e('Total')?></h3>
+	<p>
+		<?php printf(__('Themes Installed: %d'), $total_theme_count); ?>
+		<br />
+		<?php printf(__('Themes Activated: %d'), $activated_themes_count); ?>
+	</p>
 </div>
 
 <?php include('admin-footer.php'); ?>
