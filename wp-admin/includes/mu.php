0611be037f2237561310de6262deba8d0c4a53d4
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2009-01-15 17:27:50 +0000

    Fix site admin menu item, props Lunabyte, fixes #839
    git-svn-id: http://mu.svn.wordpress.org/trunk@1596 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-admin/includes/mu.php
+++ wp-admin/includes/mu.php
@@ -499,7 +499,7 @@ function wpmu_menu() {
 	global $menu, $submenu;
 
 	if( is_site_admin() ) {
-		$menu[2] = array(__('Site Admin'), '10', 'wpmu-admin.php', '', 'wp-menu-open menu-top', 'menu-dashboard', 'div');
+		$menu[2] = array(__('Site Admin'), '10', 'wpmu-admin.php', '', 'wp-menu-open menu-top', 'menu-site', 'div');
 		$submenu[ 'wpmu-admin.php' ][1] = array( __('Admin'), '10', 'wpmu-admin.php' );
 		$submenu[ 'wpmu-admin.php' ][5] = array( __('Blogs'), '10', 'wpmu-blogs.php' );
 		$submenu[ 'wpmu-admin.php' ][10] = array( __('Users'), '10', 'wpmu-users.php' );
