ca1990aba8c8eb87e57c23469133b06889032bb9
donncha <donncha@7be80a69-a1ef-0310-a953-fb0f7c49ff36>
2009-06-30 12:33:33 +0000

    Move html out of text string. Escape single quotes for translation. Fixes #1001, props yannux
    git-svn-id: http://mu.svn.wordpress.org/trunk@1803 7be80a69-a1ef-0310-a953-fb0f7c49ff36

--- wp-admin/includes/mu.php
+++ wp-admin/includes/mu.php
@@ -542,8 +542,8 @@ function wpmu_menu() {
 		if( !is_site_admin() ) {
 			unset( $menu['45'] ); // Plugins
 		} elseif ( strpos( $_SERVER[ 'PHP_SELF' ], 'wp-admin/plugins.php' ) ) {
-			$message = __( 'The plugins page is not visible to normal users. It must be <a href="wpmu-options.php#menu">activated</a> first.' );
-			$message = "<div class=\'error\'><p>$message</p></div>";
+			$message = sprintf( __( 'The plugins page is not visible to normal users. It must be activated first. %s' ), '<a href="wpmu-options.php#menu">' . __( 'Activate' ) . '</a>' );
+			$message = str_replace( "'", "\'", "<div class='error'><p>$message</p></div>" );
 			add_action( 'admin_notices', create_function( '', "echo '$message';" ) );
 		}
 		unset( $submenu[ 'plugins.php' ] );
